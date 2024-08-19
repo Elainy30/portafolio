80377b18778347689203941606bfb169659f36e0
Elainy30 <130621388+Elainy30@users.noreply.github.com>
2024-08-19 19:36:50 -0400

    Fix image

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -16,10 +16,11 @@ const Profile = () => {
         <h1>Elainy Vilorio</h1>
         <h3>DESARROLLADORA WEB FRONT-END</h3>
         <p>
-          Soy una estudiante de programación con conocimientos en el área de frontend,
-          específicamente en HTML, CSS, JavaScript, para trabajar de manera efectiva en entornos colaborativos.
-          Me considero una persona pragmática, autodidacta y curiosa, cualidades que me han permitido iniciar y
-          avanzar en mi camino en la tecnología.
+        Soy una desarrolladora web front-end con experiencia en HTML5, CSS3, JavaScript, y 
+        frameworks como React. Me apasiona crear interfaces modernas y funcionales, utilizando tecnologías como TypeScript, 
+        Tailwind CSS, y Redux. Además, domino herramientas como Git y GitHub para el control de versiones, 
+        y tengo conocimientos en Node.js y MongoDB. 
+        Mi enfoque es escribir código limpio y eficiente, siempre buscando mejorar y aprender nuevas tecnologías.
         </p>
         <a
           href="https://drive.google.com/file/d/1T73hEpO82WsBkTRZhw2O-DwD8XoIuGFB/view"
